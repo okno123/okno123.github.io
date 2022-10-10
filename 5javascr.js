21f5629c37518e7a884e2a596f326bc660d37adb
okno123 <112862842+okno123@users.noreply.github.com>
2022-10-10 19:28:07 +0300

    Update 5javascr.js

--- 5javascr.js
+++ 5javascr.js
@@ -1,1 +1,26 @@
 
+var op; 
+
+function func() {
+
+var result;
+
+var num1 = Number(document.getElementById("num1").value);
+var x=1;
+if(isNaN(num1)==true){
+    result="Некорректная цена товара";
+    x=0;
+}
+
+var num2 = Number(document.getElementById("num2").value);
+if(isNaN(num2)==true){
+    result = "Некорректная количество товара";
+    x=0;
+}
+
+if(x==1){    
+    result=num1*num2;
+}
+document.addEventListener("DOMContentLoaded", func);
+document.getElementById("result").innerHTML = result;
+}
