a3c08d7050d6123f457820afea4ded8f24ad3db7
okno123 <112862842+okno123@users.noreply.github.com>
2022-10-23 22:35:12 +0300

    Update 6java.js

--- 6java.js
+++ 6java.js
@@ -1,93 +1,103 @@
-window.addEventListener('DOMContentLoaded', function (event) {
-  let s = document.getElementsByName("myselect");
-  s[0].addEventListener("change", function(event) {
-    let select = event.target;
-    let radios = document.getElementById("myradios");
-    let svost = document.getElementById("svostva");
-    console.log(select.value);
+function updatePrice() {
+  let s = document.getElementsByName("prodType");
+  let select = s[0];
+  let price = 0;
+  let prices = getPrices();
+  let priceIndex = parseInt(select.value) - 1;
+  if (priceIndex >= 0) {
+    price = prices.prodTypes[priceIndex];
+  }
   
-    if (select.value == "1") {
-      radios.style.display = "none";
-      svost.style.display = "none";
-    }
-    if(select.value == "2") 
-    {
-        radios.style.display = "none";
-        svost.style.display = "block";
-    }
-    if(select.value == "3") 
-    {
-        svost.style.display = "none";
-        radios.style.display = "block";
-    }
+  let radioDiv = document.getElementById("radios");
+  let checkDiv = document.getElementById("checkboxes");
+  
+  if (select.value == "1") 
+  {
+    radioDiv.style.display = "none";
+    checkDiv.style.display = "none";
+  }
+  if(select.value == "2") 
+  {
+    radioDiv.style.display = "none";
+    checkDiv.style.display = "block";
+  }
+  if(select.value == "3") 
+  {
+    checkDiv.style.display = "none";
+    radioDiv.style.display = "block";
+  }
 
+  let radios = document.getElementsByName("prodOptions");
+  radios.forEach(function(radio) {
+    if (radio.checked) {
+      let optionPrice = prices.prodOptions[radio.value];
+      if (optionPrice !== undefined) {
+        price += optionPrice;
+      }
+    }
+  });
+
+
+  let checkboxes = document.querySelectorAll("#checkboxes input");
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      let propPrice = prices.prodProperties[checkbox.name];
+      if (propPrice !== undefined) {
+        price += propPrice;
+      }
+    }
   });
   
-  let r = document.querySelectorAll(".myradios input[type=radio]");
-  r.forEach(function(radio) {
+  let prodPrice = document.getElementById("prodPrice");
+  prodPrice.innerHTML = price + " рублей";
+}
+
+
+function getPrices() {
+  return {
+    prodTypes: [1000, 500, 3000],
+    prodOptions: {
+      option2: 500,
+      option3: 1000,
+    },
+    prodProperties: {
+      prop1: 100,
+      prop2: 300,
+    }
+  };
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  let radioDiv = document.getElementById("radios");
+  radioDiv.style.display = "none";
+
+  let s = document.getElementsByName("prodType");
+  let select = s[0];
+  select.addEventListener("change", function(event) {
+    let target = event.target;
+    console.log(target.value);
+    updatePrice();
+  });
+  
+  let radios = document.getElementsByName("prodOptions");
+  radios.forEach(function(radio) {
     radio.addEventListener("change", function(event) {
       let r = event.target;
       console.log(r.value);
-    });    
+      updatePrice();
+    });
   });
   
-});
+  let checkboxes = document.querySelectorAll("#checkboxes input");
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener("change", function(event) {
+      let c = event.target;
+      console.log(c.name);
+      console.log(c.value);
+      updatePrice();
+    });
+  });
 
-function func() 
-{
-    var result=0;
-    var x=1;
-    var num2 = Number(document.getElementById("num2").value);
-    if(isNaN(num2)==true){
-        result = "Некорректное количество товара";
-        x=0;
-    }
-    if(num2!=0)
-    {
-        var f=0;
-        var result2=num2*1000;
-        var sum=document.getElementsByName('svo');
-        for (var i=0; i<sum.length; i++) 
-        {
-            if (sum[i].checked) 
-            {
-                f=1;
-                result2+=100;
-            }
-        }
-        if(f==1)
-        {
-            document.getElementById("result").innerHTML = result2;
-        }
-        else
-        {
-            var f2=0;
-            var result1=num2*2000;
-            var rad=document.getElementsByName('myradio');
-            for (var i=0; i<rad.length; i++) 
-            {
-                if (rad[i].checked) 
-                {
-                    f2=1;
-                    result1+=(i+1)*20;
-                    break;
-                }
-            }
-            if(f2==1)
-            {
-                document.getElementById("result").innerHTML = result1;
-            }
-            else
-            {
-                result="Выберите опции";
-                document.getElementById("result").innerHTML = result;
-            }
-        }
-  
-    }
-    else 
-    {
-        document.getElementById("result").innerHTML = result;
-    }
-}
+  updatePrice();
+});
 
