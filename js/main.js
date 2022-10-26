2e8061f5996e2468b5fff553198b8ef31cbf39fa
okno123 <112862842+okno123@users.noreply.github.com>
2022-10-26 17:50:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*$('.single-item').slick({
+    dots: true,
+  infinite: false,
+  speed: 1500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});*/
+$('.single-item').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+});
